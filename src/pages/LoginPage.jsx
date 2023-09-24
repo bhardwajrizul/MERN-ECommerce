c2cc924f7f0cdc1cf0c8d11e679e34d6891a151f
dhingra-madhav <21BCS10568@cuchd.in>
2023-09-24 20:46:55 +0530

    added login and signup page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,21 @@
 import Panel from "../components/Panel"
+import StyleSpan from "../components/StyleSpan"
 
 export default function LoginPage() {
     return (
-        <Panel>
-            Login Form
-        </Panel>
+        <div className="flex justify-center">
+            <Panel className="flex flex-col w-1/3 px-10 py-28">
+                <StyleSpan>Login</StyleSpan>
+                <form className="flex flex-col gap-5 pt-16">
+                    <label htmlFor="email">Email:
+                        <input type="email" name="EmailId" id="0" className="border border-black block rounded-md w-full px-3 py-2" />
+                    </label>
+                    <label htmlFor="password">Password:
+                        <input type="password" name="Password" id="1" className="border border-black block rounded-md w-full px-3 py-2" />
+                    </label>
+                    <button onClick={e => e.preventDefault()} className="btn rounded mt-5">LOGIN</button>
+                </form>
+            </Panel>
+        </div>
     )
 }
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,9 +1,26 @@
 import Panel from "../components/Panel"
+import StyleSpan from "../components/StyleSpan"
+
+const input = `border border-black rounded-md block px-3 py-2 w-full `
 
 export default function SignupPage() {
     return (
-        <Panel>
-            Signup Form
-        </Panel>
+        <div className="flex justify-center">
+            <Panel className="flex flex-col w-1/3 px-10 py-20">
+                <StyleSpan>SignUp</StyleSpan>
+                <form className="flex flex-col gap-5 pt-12">
+                    <label htmlFor="name">Name:
+                        <input type="text" className={input}/>
+                    </label>
+                    <label htmlFor="email">Email:
+                        <input type="email" className={input} />
+                    </label>
+                    <label htmlFor="password">Password:
+                        <input type="password" className={input}/>
+                    </label>
+                    <button onClick={e => e.preventDefault()} className="btn rounded mt-5">SIGNUP</button>
+                </form>
+            </Panel>
+        </div>
     )
 }
